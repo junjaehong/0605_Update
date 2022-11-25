6a0899c08521423ff8466477dc7d0c511f87d2ec
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-11-25 17:27:39 +0900

    setting : devlop

---
+++ src/main/java/com/DevOOPS/barrier/BarrierApplication.java
@@ -0,0 +1,13 @@
+package com.DevOOPS.barrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevOOPS/barrier/BarrierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevOOPS.barrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
